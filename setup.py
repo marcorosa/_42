108934e6a35325e844b6a5fa8dddac035b1d23ac
Marco Rosa <marcor165@hotmail.it>
2017-02-14 19:01:04 +0100

    Fix setup README

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-with open('README.md') as README:
+with open('README.rst') as README:
     long_description = README.read()
     long_description = long_description[long_description.index('Description'):]
 
 setup(name='python-42',
-      version='0.1.1',
+      version='0.1.2',
       description='Decorator that runs everything (in its way)',
       long_description=long_description,
       url='http://github.com/marcorosa/_42',
